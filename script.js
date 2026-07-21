0aea9e7cb8d225750aefb70b3a7566c1955cb82b
logcc <paulbestava@gmail.com>
2026-07-21 15:56:50 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+
+window.addEventListener(
+"scroll",
+()=>{
+
+let nav=document.querySelector("nav");
+
+
+if(window.scrollY>50){
+
+nav.style.background="#020617";
+
+}
+
+else{
+
+nav.style.background="#0f172a";
+
+}
+
+
+}
+
+);
